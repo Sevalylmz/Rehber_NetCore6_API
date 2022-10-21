b7af4a18f2fb2b5b808feb860fdd8e740dc9edde
Seval <107204729+Sevalylmz@users.noreply.github.com>
2022-10-21 11:56:06 +0300

    created report controller.Update README.md

---
+++ Rehber/Controllers/ReportsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rehber.DataAccess.Interfaces;
+
+namespace Rehber.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly ILocationDAL _locationDAL;
+
+        public ReportsController(ILocationDAL locationDAL)
+        {
+            _locationDAL = locationDAL;
+        }
+
+        // En çok -> En az olacak şekilde konumlarının sayılarıyla listelenmes
+
+        [HttpGet("getLocationReport")]
+        public IActionResult GetLocationReport()
+        {
+            var allLocation = _locationDAL.GetDefaults(a => a.ID != 0);
+            var result =
+                     from e in allLocation
+                     group e by e.Value into g
+                        select new { Location = g.Key, Total = g.Count() };
+
+            return Ok(result);
+
+        }
+    }
+}
